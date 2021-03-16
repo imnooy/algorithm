e53174af135c7599c6d4fc8b77414ed19126f3a4
imnooy <imnoooy@gmail.com>
2021-03-16 13:55:35 +0900

    string

--- BOJ/String/String/[1316번] 그룹 단어 체커.cpp
+++ BOJ/String/String/[1316번] 그룹 단어 체커.cpp
@@ -6,7 +6,7 @@ using namespace std;
 int main() {
 	set<char> s;
 	bool isgroup, isnot;
-	int n;
+	int n, j;
 	int cnt = 0;
 	cin >> n;
 
@@ -14,24 +14,37 @@ int main() {
 	string *sarr = new string[n];
 
 	for (int i = 0; i < n; i++) {
+		s.clear();
 		cin >> sarr[i];
-		for (int j = 0; j < sarr[i].size(); j++) {
-			s.insert(sarr[i].at(j));
+		for (int k = 0; k < sarr[i].size(); k++) {
+			s.insert(sarr[i].at(k));
 		}
 		if (s.size() == sarr[i].size()) arr[i] = true;
 		else {
 			isnot = false;
-			for (int j = 0; j < sarr[i].size()-1; j++) {
-				if ((sarr[i].at(j) == sarr[i].at(j + 1)) && (s.find(sarr[i].at(j))!=s.end())) { //연속되는 문자가 있고 set에 있을 때
-					sarr[i] = sarr[i].substr(j + 1, sarr[i].size() - 1); //set에서 지우지 않고 한글자 잘라냄
-				}
-				else if ((sarr[i].at(j) != sarr[i].at(j + 1)) && (s.find(sarr[i].at(j)) != s.end())) { //연속되는 문자 없고 set에 있을 때
-					sarr[i] = sarr[i].substr(j + 1, sarr[i].size() - 1);
-					s.erase(s.find(sarr[i].at(j)));
-				}
+			while (1) {
+				j = 0;
+				if (sarr[i].size() > 1) {
+					if ((sarr[i].at(j) == sarr[i].at(j + 1)) && (s.find(sarr[i].at(j)) != s.end())) { //연속되는 문자가 있고 set에 있을 때
+						sarr[i] = sarr[i].substr(j + 1, sarr[i].size() - 1); //set에서 지우지 않고 한글자 잘라냄
+					}
+					else if ((sarr[i].at(j) != sarr[i].at(j + 1)) && (s.find(sarr[i].at(j)) != s.end())) { //연속되는 문자 없고 set에 있을 때
+						s.erase(s.find(sarr[i].at(j)));
+						sarr[i] = sarr[i].substr(j + 1, sarr[i].size() - 1);
+					}
 
-				else {
+					else {
+						isnot = true;
+						break;
+					}
+				}
+				else if (sarr[i].size() == 1 && (s.find(sarr[i].at(j)) != s.end())) { //길이가 1이고 set에 있을 때
+					isnot = false;
+					break;
+				}
+				else if (sarr[i].size() == 1 && (s.find(sarr[i].at(j)) == s.end())) { //길이가 1이고 set에 없을 때
 					isnot = true;
+					break;
 				}
 			}
 			if(isnot==false) arr[i] = true;
@@ -44,6 +57,9 @@ int main() {
 	}
 
 	cout << cnt;
+
+	delete[] arr;
+	delete[] sarr;
 
 	return 0;
 }
