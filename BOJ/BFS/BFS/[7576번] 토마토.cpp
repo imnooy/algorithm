4b73fad970fe9d552f187aed340cfea36c44da6b
imnooy <imnoooy@gmail.com>
2021-04-06 20:25:47 +0900

    bfs(answer)

--- BOJ/BFS/BFS/[7576번] 토마토.cpp
+++ BOJ/BFS/BFS/[7576번] 토마토.cpp
@@ -14,23 +14,25 @@ int main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 
-	int a;
 	queue<pair<int, int>> q;
-	int n, m;
+	int m, n;
 	pair<int, int> p;
 	int xx, yy;
 	bool isCorrect = true;
-	int ans = 0;
 
-	cin >> n >> m;
+	cin >> m >> n;
 
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < m; j++) {
 			cin >> arr[i][j];
-			if (arr[i][j] == 1) q.push({ i, j });
-			if (arr[i][j] == 0) dis[i][j] = -1;
+			if (arr[i][j] == 1) {
+				q.push({ i, j });
+				dis[i][j] = 1;
+			}
+			if (arr[i][j] == -1) dis[i][j] = -1;
 		}
 	}
+
 
 	while (!q.empty()) {
 		p = q.front();
@@ -41,20 +43,25 @@ int main() {
 			yy = p.y + dy[i];
 
 			if (xx < 0 || xx >= n || yy < 0 || yy >= m) continue;
-			if (dis[xx][yy] >= 0) continue;
+			if (dis[xx][yy] == -1) continue;
 
-			dis[xx][yy] = dis[p.x][p.y] + 1;
-			q.push({ xx, yy });
+			if (dis[xx][yy] > 0) {
+				dis[xx][yy] = min(dis[xx][yy], dis[p.x][p.y] + 1);
+			}
+			else if (dis[xx][yy] == 0) {
+				dis[xx][yy] = dis[p.x][p.y] + 1;
+				q.push({ xx, yy });
+			}
 		}
 	}
+	int ans = 0;
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < m; j++) {
-			if (dis[i][j] == -1) {
-				isCorrect = false;
-			}
+			if (dis[i][j] == 0) isCorrect = false;
 			ans = max(ans, dis[i][j]);
 		}
 	}
-	if(isCorrect) cout << ans;
+
+	if (isCorrect) cout << ans - 1;
 	else cout << "-1";
 }
