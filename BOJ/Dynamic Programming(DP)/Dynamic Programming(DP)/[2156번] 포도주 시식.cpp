47db4fc4b6960055f78fe6d44d332b6083b4e25c
DESKTOP-R245OE3\imnoo <imnoooy@gmail.com>
2022-04-08 00:30:59 +0900

    dp

---
+++ BOJ/Dynamic Programming(DP)/Dynamic Programming(DP)/[2156번] 포도주 시식.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int t, n;
+long long arr[1000001];
+long long d[1000001];
+
+int main() {
+	cin.tie(0);
+	ios::sync_with_stdio(0);
+
+	d[1] = 1;
+	d[2] = 2;
+	d[3] = 7;
+
+	for (int i = 4; i <= 1000000; i++) {
+		d[i] = (d[i - 3] % 1000000009) + (d[i - 2] % 1000000009) + (d[i - 1] % 1000000009) % 1000000009;
+	}
+
+	cin >> t;
+	for (int i = 0; i < t; i++) {
+		cin >> n;
+		cout << d[n] % 1000000009;
+	}
+
+	return 0;
+}
