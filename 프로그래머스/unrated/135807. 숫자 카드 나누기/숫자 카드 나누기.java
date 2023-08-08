1c198f3eb2ca648a4202883bc0f08d3cf3002d55
Yoonmi <75800620+imnooy@users.noreply.github.com>
2023-08-08 21:55:39 +0900

    [unrated] Title: 숫자 카드 나누기, Time: 0.43 ms, Memory: 72.4 MB -BaekjoonHub

--- 프로그래머스/unrated/135807. 숫자 카드 나누기/숫자 카드 나누기.java
+++ 프로그래머스/unrated/135807. 숫자 카드 나누기/숫자 카드 나누기.java
@@ -2,39 +2,40 @@ import java.util.*;
 
 class Solution {
     public int gcd(int a, int b) {
-        if(a==0) return b;
-        return gcd(b%a, a);
+        if(a == 0) return b;
+        return gcd(b % a, a);
     }
+    
     public int solution(int[] arrayA, int[] arrayB) {
         int answer = 0;
-        int a=0;
-        int b=0;
-        
         Arrays.sort(arrayA);
         Arrays.sort(arrayB);
         
-        for(int i=0; i<arrayA.length; i++) {
-            a= gcd(a, arrayA[i]);  
+        int a = 0;
+        int b = 0;
+        
+        for(int i = 0; i < arrayA.length; i++) {
+            a = gcd(a, arrayA[i]);  //a 최댓값
         }
         
-        for(int i=0; i<arrayB.length; i++) {
-            b=gcd(b, arrayB[i]);
+        for(int i = 0; i < arrayB.length; i++) {
+            b = gcd(b, arrayB[i]); // b 최댓값
         }
         
-        for(int i=0; i<arrayB.length; i++) {
-            if(arrayB[i]%a==0) {
-                a=1;
-                break; //a는 안된다
+        for(int i = 0; i < arrayB.length; i++) {
+            if(arrayB[i] % a==0) {
+                a = 0;
+                break;
             }
         }
-        for(int i=0; i<arrayA.length; i++) {
-            if(arrayA[i]%b==0) {
-                b=1;
-                break; //b는 안된다
+        for(int i = 0; i < arrayA.length; i++) {
+            if(arrayA[i] % b == 0) {
+                b = 0;
+                break;
             }
         }
         
-        if(a==1 && b==1) return 0;
+        if(a == 0 && b == 0) return 0;
         return Math.max(a, b);
     }
 }
